f6e875fa5089b41c7e3c0e6c4ab3c8031ad955d1
Ayanrinde Olutayo Stephen <olutayostephen@gmail.com>
2024-03-07 11:28:30 +0100

    updated

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,11 +1,13 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import axios from 'axios'
 import { useNavigate } from 'react-router-dom'
 const Dashboard = () => {
   let url = "https://node-modularized.onrender.com/user/dashboard"
   let token = localStorage.token
   let navigate = useNavigate()
- 
+  const [mfile, setmfile] = useState("")
+  
+
   useEffect(()=>{
     axios.get(url, {
       headers: {
@@ -26,9 +28,15 @@ const Dashboard = () => {
     navigate('/signin')
   }
 
+  const sfile = (e)=>{
+    console.log(mfile)
+  }
+
   return (
     <div>
       <h1> WELCOME TO DASHBOARD</h1>
+      <input type="file" onChange={(e)=>setmfile(e.target.files)} />
+      <button onClick={sfile}> Upload </button>
       <button onClick={signOut}>sign out</button>
     </div>
   )
--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -12,31 +12,43 @@ const Signup = () => {
     const [password, setpassword] = useState("")
     const [responsemessage, setresponsemessage] = useState("")
     const [status, setstatus] = useState()
+    const [myfile, setmyfile]=useState("")
     let url = "https://node-modularized.onrender.com/user/register"
     //let url = "http://localhost:7000/user/register"
     
     const registerUser=()=>{
-        axios.post(url, {firstname:name, lastname, email, password})
+      console.log(myfile);
+        axios.post(url, {firstname:name, lastname, email, password, myfile})
         .then((response)=>{
           console.log(response)
           
           if (response.data.status)
-          {alert("HURRAY SignUp Successful"); setresponsemessage("SigUp Successful"); setstatus(response.data.status); navigate("/signin");}
-          
-          else if (!response.data.status) {alert(response.data.message); setresponsemessage("SigUp Failed" + "/n" + response.data.message); setstatus(response.data.status); setresponsemessage(response.data.message); console.log(response.data.message); }
+          {alert("HURRAY SignUp Successful"); navigate("/signin"); console.log(response.data.message); setresponsemessage("SigUp Successful"); setstatus(true); }
+          else{alert("Signup failed")}
+          // else if (!response.data.status) {alert(response.data.message); setresponsemessage("SigUp Failed" + "/n" + response.data.message); setstatus(false); setresponsemessage(response.data.message); console.log(response.data.message); }
         })
-
     }
+      //CONVERTING IMAGE TO BASE 64 STRING
+      const changeFile = (e)=>{
+      console.log(e.target.files)
+      let myImage = e.target.files[0]
+      let reader = new FileReader()
+      reader.readAsDataURL(myImage)
+      reader.onload = ()=>{ setmyfile(reader.result); console.log(reader.result)}
+  }
   return (
     <>
         <div> {responsemessage} </div>
-        {
-          status? <div className='alert alert-success'> </div> : <div className='alert alert-success'> </div> 
-        }<form className='form form-control'>
+        {/* {
+          status? <div className='alert alert-success'> </div> : <div className='alert alert-danger'> </div> 
+        } */}
+        <form className='form form-control'>
         <input  type='text' placeholder='firstname' onChange={(e)=>setname(e.target.value)} />
         <input type='text' placeholder='lastname' onChange={(e)=>setlastname(e.target.value)} />
         <input type='email' placeholder='email' onChange={(e)=>setemail(e.target.value)} />
         <input type='password' placeholder='password' onChange={(e)=>setpassword(e.target.value)} />
+        <input type="file" onChange={(e)=>changeFile(e)} />
+        {/* <input type="file" onChange={(e)=>setmyfile(e.target.files)} /> */}
         <button onClick={registerUser}> Register </button>
         </form>
     </>
